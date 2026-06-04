a5c86b2a1d42ff21b6cbdd278a45944657481703
tlstjrgml <zaqbnh741@gmail.com>
2026-06-04 17:07:29 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/moa/backend/MoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaBackendApplication.class, args);
+	}
+
+}
